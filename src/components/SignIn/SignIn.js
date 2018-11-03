5ecd15d484e94f09e41d9aed335be3d8fa00704c
virginia974 <39102527+virginia974@users.noreply.github.com>
2018-11-03 21:30:49 +0000

    changed db link

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -16,7 +16,7 @@ class SignIn extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://still-bastion-48569.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
